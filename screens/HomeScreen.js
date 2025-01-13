71c03e5016cb08536e1ddb70ef63a9f2cdc4d456
SirRazac <Fabian.Kipp@web.de>
2025-01-13 22:42:41 +0100

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Das ist der Startbildschirm!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#555',
+    },
+});
